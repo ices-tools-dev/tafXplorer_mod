5b9b51352b03b0f2a02421b13dd39b5e08ff5f5e
Colin Millar <colinpmillar@gmail.com>
2024-03-13 16:24:33 +0100

    rename selected -> selected_map to highlight this is tracking what is selected on the map

--- shiny_mod_map_selector.R
+++ shiny_mod_map_selector.R
@@ -26,31 +26,33 @@ mod_map_selector_server <- function(id) {
 
     proxy_map <- leafletProxy("map_selector")
 
-    # create empty character vector to hold all click ids
-    selected <- reactiveValues(groups = character())
+    # create empty character vector to hold map selected locations
+    selected_map <- reactiveValues(groups = character())
 
     observeEvent(input$map_selector_shape_click, {
+      # ecoregion added
       if (input$map_selector_shape_click$group == "Eco_regions") {
         proxy_map %>% showGroup(input$map_selector_shape_click$id)
 
-        selected$groups <- c(selected$groups, input$map_selector_shape_click$id)
+        selected_map$groups <- c(selected_map$groups, input$map_selector_shape_click$id)
       }
 
+      # ecoregion removed
       if (input$map_selector_shape_click$group %in% vocabs$ecoregions) {
         proxy_map %>% hideGroup(input$map_selector_shape_click$group)
 
-        selected$groups <- setdiff(selected$groups, input$map_selector_shape_click$group)
+        selected_map$groups <- setdiff(selected_map$groups, input$map_selector_shape_click$group)
       }
 
-      updateVirtualSelect("selected_locations", selected = selected$groups)
+      updateVirtualSelect("selected_locations", selected = selected_map$groups)
     })
 
     observeEvent(input$selected_locations,
       {
-        removed <- setdiff(selected$groups, input$selected_locations)
-        added <- setdiff(input$selected_locations, selected$groups)
+        removed <- setdiff(selected_map$groups, input$selected_locations)
+        added <- setdiff(input$selected_locations, selected_map$groups)
 
-        selected$groups <- input$selected_locations
+        selected_map$groups <- input$selected_locations
 
         if (length(removed)) {
           proxy_map %>% hideGroup(removed)
