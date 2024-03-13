19d81453f276d91095fa673515fb76b62a904318
Colin Millar <colinpmillar@gmail.com>
2024-03-13 13:24:05 +0100

    delete shiny folder

--- shiny/data/www/detect_click2.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('body').on('click', 'a', function(evt){
-        Shiny.setInputValue('clicked_text', evt.target.id);
-    });
-    })
--- shiny/utilities_JS_callbacks.R
+++
@@ -1,6 +0,0 @@
-callback <- c(
-  "$('input[name=rdbtn]').on('click', function(){",
-  "  var value = $('input[name=rdbtn]:checked').val();",
-  "  Shiny.setInputValue('rdbtn', value);",
-  "});"
-)
