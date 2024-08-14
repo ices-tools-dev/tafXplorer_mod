7cce67293a95f877cbe9f5c44578a1814c1fd493
Luca Lamoni <lucalamoni@gmail.com>
2024-08-14 14:09:35 +0200

    clean up

--- shiny_mod_map_selector.R
+++ shiny_mod_map_selector.R
@@ -4,8 +4,6 @@ mod_map_selector_ui <- function(id) {
     page_fillable(
     layout_columns(
       card(
-        #4,
-        # tags$style(type = "text/css", "#logo {height: auto !important; margin-top: 10px;  padding-bottom: 10px; }"),
         tags$img(id = "logo", src = "TAFXplorer blue.png"),
         leaflet::leafletOutput(ns("map_selector")),
         virtualSelectInput(
