851cbdbe12076a2ef7182d7fba8c8a2e12c3f75c
Luca Lamoni <lucalamoni@gmail.com>
2024-08-16 13:13:29 +0200

    cleaned up

--- shiny_mod_login.R
+++ shiny_mod_login.R
@@ -40,87 +40,87 @@ mod_login_server <- function(id) {
 
 
 
-library(shiny)
-library(shinydashboard)
+# library(shiny)
+# library(shinydashboard)
 
-Logged = FALSE
-my_username <- "test"
-my_password <- "test"
+# Logged = FALSE
+# my_username <- "test"
+# my_password <- "test"
 
-js <- '
-$(document).keyup(function(event) {
-  if ($("#password").is(":focus") && (event.keyCode == 13)) {
-      $("#login").click();
-  }
-});
-'
+# js <- '
+# $(document).keyup(function(event) {
+#   if ($("#password").is(":focus") && (event.keyCode == 13)) {
+#       $("#login").click();
+#   }
+# });
+# '
 
-ui <- dashboardPage(skin = "blue",
-                    dashboardHeader(title = "Dashboard"),
-                    dashboardSidebar(),
-                    dashboardBody("Test",
-                                  verbatimTextOutput("dataInfo")
-                    )
-)
+# ui <- dashboardPage(skin = "blue",
+#                     dashboardHeader(title = "Dashboard"),
+#                     dashboardSidebar(),
+#                     dashboardBody("Test",
+#                                   verbatimTextOutput("dataInfo")
+#                     )
+# )
 
-server = function(input, output, session) {
+# server = function(input, output, session) {
 
-  values <- reactiveValues(authenticated = FALSE)
+#   values <- reactiveValues(authenticated = FALSE)
 
-  # Return the UI for a modal dialog with data selection input. If 'failed' 
-  # is TRUE, then display a message that the previous value was invalid.
-  dataModal <- function(failed = FALSE) {
-    modalDialog(
-      tags$script(HTML(js)),
-      textInput("username", "Username:"),
-      passwordInput("password", "Password:"),
-      footer = tagList(
-        # modalButton("Cancel"),
-        actionButton("login", "Login")
-      )
-    )
-  }
+#   # Return the UI for a modal dialog with data selection input. If 'failed' 
+#   # is TRUE, then display a message that the previous value was invalid.
+#   dataModal <- function(failed = FALSE) {
+#     modalDialog(
+#       tags$script(HTML(js)),
+#       textInput("username", "Username:"),
+#       passwordInput("password", "Password:"),
+#       footer = tagList(
+#         # modalButton("Cancel"),
+#         actionButton("login", "Login")
+#       )
+#     )
+#   }
 
-  # Show modal when button is clicked.  
-  # This `observe` is suspended only whith right user credential
+#   # Show modal when button is clicked.  
+#   # This `observe` is suspended only whith right user credential
 
-  obs1 <- observe({
-    showModal(dataModal())
-  })
+#   obs1 <- observe({
+#     showModal(dataModal())
+#   })
 
-  # When OK button is pressed, attempt to authenticate. If successful,
-  # remove the modal. 
+#   # When OK button is pressed, attempt to authenticate. If successful,
+#   # remove the modal. 
 
-  obs2 <- observe({
-    req(input$login)
-    isolate({
-      Username <- input$username
-      Password <- input$password
-    })
-    Id_username <- which(my_username == Username)
-    Id_password <- which(my_password == Password)
-    if (length(Id_username) > 0 & length(Id_password) > 0) {
-      if (Id_username == Id_password) {
-        Logged <<- TRUE
-        values$authenticated <- TRUE
-        obs1$suspend()
-        removeModal()
+#   obs2 <- observe({
+#     req(input$login)
+#     isolate({
+#       Username <- input$username
+#       Password <- input$password
+#     })
+#     Id_username <- which(my_username == Username)
+#     Id_password <- which(my_password == Password)
+#     if (length(Id_username) > 0 & length(Id_password) > 0) {
+#       if (Id_username == Id_password) {
+#         Logged <<- TRUE
+#         values$authenticated <- TRUE
+#         obs1$suspend()
+#         removeModal()
 
-      } else {
-        values$authenticated <- FALSE
-      }     
-    }
-  })
+#       } else {
+#         values$authenticated <- FALSE
+#       }     
+#     }
+#   })
 
 
-  output$dataInfo <- renderPrint({
-    if(values$authenticated){
-      "OK!!!!!"
-    } else {
-      "You are NOT authenticated"
-    }
-  })
+#   output$dataInfo <- renderPrint({
+#     if(values$authenticated){
+#       "OK!!!!!"
+#     } else {
+#       "You are NOT authenticated"
+#     }
+#   })
 
-}
+# }
 
-shinyApp(ui,server)
+# shinyApp(ui,server)
