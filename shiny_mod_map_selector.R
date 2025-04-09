4bb3ab30f534bdc63de273cc08a7e164c24c4a70
Luca Lamoni <lucalamoni@gmail.com>
2025-04-09 15:52:38 +0200

    first attempt at icons working

--- shiny_mod_map_selector.R
+++ shiny_mod_map_selector.R
@@ -17,7 +17,7 @@ mod_map_selector_ui <- function(id) {
       params = list(
         year = list(inputId = "year", label = "Assessment year:"),
         stockCode = list(inputId = "stockCode", label = "Stock code:"),
-        species = list(inputId = "species", label = "Common name:"),
+        species = list(inputId = "CommonName", label = "Common name:"),
         expertGroup = list(inputId = "expertGroup", label = "Expert group:"),
         dataCategory = list(inputId = "dataCategory", label = "Data category:")
       ),
@@ -96,7 +96,10 @@ mod_map_selector_server <- function(id, token) {
 
     repo_list <- reactive({
       req(input$selected_locations)
-      stock_list_long <- getListStockAssessments()
+      # stock_list_long <- getListStockAssessments()
+
+      stock_list_long <- jsonlite::fromJSON("www/test_list.json")
+      
       stock_list_long <- purrr::map_dfr(
         .x = input$selected_locations,
         .f = function(.x) stock_list_long %>% dplyr::filter(str_detect(ecoregion, .x))
@@ -111,7 +114,54 @@ mod_map_selector_server <- function(id, token) {
             RepoUrl = paste0("<a href='", gitHubUrl, "' target='_blank'>Link")
             # stock_description = purrr::map_chr(StockKeyLabel, .f = ~ access_sag_data_local(.x, input$selected_years)$StockDescription[1]),
             # stock_location = parse_location_from_stock_description(stock_description)
-          )
+          ) %>% 
+          mutate(
+                Diagnostics = mapply(function(df, mf, of, rf, sr) {
+                  paste(
+                    # paste0(rep('<i class="fa-solid fa-database" title="Data file"></i>', df), collapse = " "),
+                    if (df == "1") {
+                      '<img src="data_green.png" title="Data files correct" height="20px"/>'
+                    } else if (df == "2") {
+                      '<img src="data_red.png" title="Data files warning/error" height="20px"/>'
+                    } else if (df == "3") {
+                      '<img src="data_grey.png" title="No data files" height="20px"/>'
+                    },                    
+                    # paste0(rep('<i class="fa-solid fa-chart-diagram" title="Model file"></i>', mf), collapse = " "),
+                    if (mf == "1") {
+                      '<img src="model_green.png" title="Model files correct" height="20px"/>'
+                    } else if (mf == "2") {
+                      '<img src="model_red.png" title="Model files warning/error" height="20px"/>'
+                    } else if (mf == "3") {
+                      '<img src="model_grey.png" title="No model files" height="20px"/>'
+                    },
+                    # paste0(rep('<i class="fa-solid fa-file-export" title="Output file"></i>', of), collapse = " "),
+                    if (of == "1") {
+                      '<img src="output_green.png" title="Output files correct" height="20px"/>'
+                    } else if (of == "2") {
+                      '<img src="output_red.png" title="Output files warning/error" height="20px"/>'
+                    } else if (of == "3") {
+                      '<img src="output_grey.png" title="No output files" height="20px"/>'
+                    },
+                    # paste0(rep('<i class="fa-solid fa-file-import" title="Report file"></i>', rf), collapse = " "),
+                    if (rf == "1") {
+                      '<img src="report_green.png" title="Report files correct" height="20px"/>'
+                    } else if (rf == "2") {
+                      '<img src="report_red.png" title="Report files warning/error" height="20px"/>'
+                    } else if (rf == "3") {
+                      '<img src="report_grey.png" title="No report files" height="20px"/>'
+                    },
+                    # paste0(rep('<i class="fa-solid fa-chart-line" title="Output file"></i>', of), collapse = " "),
+                    # paste0(rep('<i class="fa-solid fa-file-contract" title="Report file"></i>', rf), collapse = " "),
+                    if (sr == "TRUE") {
+                      '<img src="check.png" title="Run successful" height="20px"/>'
+                    } else {
+                      '<img src="delete.png" title="Run failed" height="20px"/>'
+                    },
+                    sep = " "
+                  )
+                }, DataFiles, ModelFiles, OutputFiles, ReportFiles, SuccessfulRun)
+              )
+
       }
     })
 
@@ -119,7 +169,7 @@ mod_map_selector_server <- function(id, token) {
       id = "my-filters",
       data = repo_list(),
       vars = reactive(c(
-        "year", "stockCode", "species", "expertGroup", "dataCategory"
+        "year", "stockCode", "CommonName", "expertGroup", "dataCategory", "Diagnostics"
       ))
     )
 
@@ -135,10 +185,11 @@ mod_map_selector_server <- function(id, token) {
           "year",
           # "EcoRegion",
           # "icon",
-          "species",
+          "CommonName",
           "expertGroup",
           "dataCategory",
-          "RepoUrl"
+          "RepoUrl",
+          "Diagnostics"
         ) %>%
         rename(
           # "Select" = Select,
@@ -146,7 +197,7 @@ mod_map_selector_server <- function(id, token) {
           "Year" = year,
           # "Ecoregion" = EcoRegion,
           # " " = icon,
-          "Common name" = species,
+          "Common name" = CommonName,
           "Expert group" = expertGroup,
           "Data category" = dataCategory,
           "Repo Url" = RepoUrl
@@ -172,6 +223,11 @@ mod_map_selector_server <- function(id, token) {
           "Repo Url" = reactable::colDef(
             html = TRUE,
             filterable = FALSE
+          ),
+          Diagnostics = colDef(
+            name = "Diagnostics",
+            cell = function(value) HTML(value),
+            html = TRUE
           )
         ),
         theme = reactableTheme(
